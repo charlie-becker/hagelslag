b862fd877da9a158561d84526f3c609407708c8e
Charlie Becker <charles11becker@gmail.com>
2021-03-01 16:34:02 -0700

    Add HRRR AWN config and run script

---
+++ run_hsdata_hrrr_aws.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -l
+#SBATCH --job-name=hsdata_wrfrt
+#SBATCH --account=NAML0001
+#SBATCH --ntasks=24
+#SBATCH --cpus-per-task=1
+#SBATCH --time=00:20:00
+#SBATCH --partition=dav
+#SBATCH --mem=64G
+#SBATCH --output=aws_hsdata.out
+#SBATCH --mail-type=ALL
+conda activate hagelslag
+python -u /glade/scratch/cbecker/hagelslag/bin/hsdata /glade/scratch/cbecker/hagelslag/config/HRRR_AWS.config -p 1
